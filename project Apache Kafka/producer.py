b2b2cacf56c8d8591652701914a96195842f0708
HeinrichEdelmann <heinrich.edelmann@gmail.com>
2023-07-30 18:21:57 +0200

    Update producer.py

--- project Apache Kafka/producer.py
+++ project Apache Kafka/producer.py
@@ -4,8 +4,8 @@ from kafka import KafkaProducer
 
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                         value_serializer=lambda x: 
-                         dumps(x).encode('utf-8')
+              api_version=(0,11,5),
+              value_serializer=lambda x: dumps(x).encode('utf-8'))
 
 for e in range(1000):
     data = {'number' : e}
