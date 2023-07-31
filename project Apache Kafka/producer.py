7c5d85da5c22ce03c952618d92d7884d8a970f81
HeinrichEdelmann <heinrich.edelmann@gmail.com>
2023-07-31 17:26:33 +0200

    Update producer.py

--- project Apache Kafka/producer.py
+++ project Apache Kafka/producer.py
@@ -7,7 +7,7 @@ producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
               api_version=(0,11,5),
               value_serializer=lambda x: dumps(x).encode('utf-8'))
 
-for e in range(1000):
+for e in range(100):
     data = {'number' : e}
     producer.send('numtest', value=data)
     sleep(5)
