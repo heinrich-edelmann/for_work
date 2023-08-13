fe11fbbf1589aa146c9a82b317f1740263f88695
HeinrichEdelmann <heinrich.edelmann@gmail.com>
2023-08-13 17:29:31 +0200

    Update matriz.R

--- learning R/matriz.R
+++ learning R/matriz.R
@@ -3,9 +3,9 @@
 ############################################
 
 # Conjunto de registros com linhas e colunas, 
-# contendo somente números ou somente caracteres. 
+# contendo somente numeros ou somente caracteres. 
 
-?matrix
+?matrix  # a interrogacao ? em frente ao nome de algum elemento da a descricao desse elemento, seus parametros e como usa-lo, ou da pra ir na sessao Help e digitar o nome do elemento
 ?factor
 
 matriz <- matrix(c(1,5,10,30,15,8),nrow=3,ncol=2,byrow=TRUE)
@@ -14,10 +14,16 @@ print(matriz)
 matriz <- matrix(c(4,8,12,16,20,24),nrow=3,ncol=2,byrow=FALSE)
 print(matriz)
 
-matriz <- matrix(c(4,8,12,16,20,24),nrow=2,ncol=3,byrow=TRUE)
+matriz <- matrix(c(4,8,12,16,20,24),nrow=3,ncol=2,byrow=TRUE)
 print(matriz)
 
-matriz [1,3]
+
+# com o parametro byrow=TRUE a matriz Ã© preechinda porlinha 
+# com byrow=FALSE a matriz Ã© preenchida por coluna
+
+matriz [1,2]
+
+?rbind # â†’ binds vectors, matrizes or data-frames
 
 vetorA <- c(2,5,8)
 vetorB <- c(3,6,9)
@@ -26,30 +32,40 @@ matriz2
 
 matriz2 [2,1]
 
+matriz3=matrix(2:9, ncol = 2, byrow = TRUE)
+matriz3
+
 matriz3=matrix(2:9, ncol = 2)
 matriz3
 
 
-# Número de linhas e colunas.
-dim(matriz3)
-nrow(matriz3)
-ncol(matriz3)
+# Numero de linhas e colunas.
+dim(matriz3) # dim() da as dimensoes
+nrow(matriz3) # n de linhas
+ncol(matriz3) # n de colunas
 
 # Nomear linhas e colunas
-dimnames(matriz3) <- list(c("Linha1","Linha2","Linha3","Linha4"),
+dimnames(matriz3) <- list(c("Linha1","Linha2","Linha3","Linha4"), #dimnames() renomeia as linhas/colunas
                           c("Coluna1", "Coluna2"))
-matriz3
+matriz3             # â†‘â†‘ os valores em list foram dados bem na sequencia "linhas" e depois "colunas"
+                    # list(linhas,colunas)
+
+matriz4=matrix(2:13, nrow = 4, byrow=TRUE,
+               dimnames = list(c("Linha1","Linha2","Linha3","Linha4"),
+                               c("Coluna1", "Coluna2","Coluna3")))
+matriz4
+
 
 matriz4=matrix(2:13, nrow = 4, byrow=FALSE,
                dimnames = list(c("Linha1","Linha2","Linha3","Linha4"),
                                c("Coluna1", "Coluna2","Coluna3")))
 matriz4
 
-# Produto de um número por uma matriz
+# Produto de um n?mero por uma matriz
 produto <- 2 * matriz4
 produto
 
-# Soma ou subtração de matrizes
+# Soma ou subtra??o de matrizes
 matriz5 = matrix(c(1,5,15,8),nrow=2,ncol=2,byrow=TRUE)
 matriz5
 matriz6 = matrix(c(2,4,6,10),nrow=2,ncol=2,byrow=TRUE)
@@ -62,12 +78,12 @@ subtracao = matriz5-matriz6
 subtracao
 
 
-# Multiplicação Matricial
+# Multiplicacao Matricial
 
-produto_matriz = matriz5 %*% matriz6
+produto_matriz = matriz5 %*% matriz6 # pra multiplicacao matricial
 produto_matriz
 
-# Média das linhas ou colunas
+# Media das linhas ou colunas
 matriz5 = matrix(c(1,5,15,8),nrow=2,ncol=2,byrow=TRUE)
 matriz5
 
@@ -85,7 +101,7 @@ soma_colunas <- colSums(matriz5)
 soma_colunas
 
 # Matriz com caracteres
-matriz7 = matrix(c("segunda","terça","quarta","quinta"),nrow=2,ncol=2,byrow=FALSE)
+matriz7 = matrix(c("segunda","terca","quarta","quinta"),nrow=2,ncol=2,byrow=FALSE)
 matriz7
 
 
